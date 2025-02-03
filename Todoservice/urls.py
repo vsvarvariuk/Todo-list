2cefbccb4465b2e57eac62aeeb78efd9dd70f8b7
vavarvariuk <vasya.varvariuk.27@gmail.com>
2025-02-03 13:48:07 +0200

    create views and templates for tasks create update and delete

--- Todoservice/urls.py
+++ Todoservice/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from Todoservice.views import TaskListView, TaskCreateView
+from Todoservice.views import (TaskListView,
+                               TaskCreateView,
+                               TaskUpdateView,
+                               TaskDeleteView)
 
 urlpatterns = [
 
     path("",TaskListView.as_view(),name="task-list"),
-    path("",TaskCreateView.as_view(),name="task-create")
+    path("create-task/",TaskCreateView.as_view(),name="task-create"),
+    path("update-task/<int:pk>/",TaskUpdateView.as_view(),name="task-update"),
+    path("delete-task/<int:pk>/",TaskDeleteView.as_view(),name="task-delete")
 ]
 app_name = "Todo"
--- Todoservice/views.py
+++ Todoservice/views.py
@@ -13,3 +13,16 @@ class TaskCreateView(generic.CreateView):
     fields = "__all__"
     success_url = reverse_lazy("service/list_task.html")
     template_name = "service/task_create.html"
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    template_name = "service/task_update.html"
+    success_url = reverse_lazy("service/list_task.html")
+
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "service/task_delete.html"
+    success_url = reverse_lazy("service/list_task.html")
